c307fe99fbbb88eca8a0241b626ebed022727b0e
Sanchit <sanchit.sharma98@gmail.com>
2024-08-04 14:35:47 +0530

    Fix dockerfile and docker-compose Fix predict.py

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,16 @@
-FROM mcr.microsoft.com/azureml/minimal-ubuntu20.04-py38-cpu-inference:latest
+FROM python:3.7-slim
 
-USER root
-RUN apt update && apt install -y python3-dev python3-pip nano && rm -rf /opt/miniconda
+#USER root
+RUN apt update && apt install -y  python3-pip vim # && rm -rf /opt/miniconda
 
-USER dockeruser
+#USER dockeruser
 COPY requirements.txt .
-RUN /usr/bin/python3 -m pip install -r requirements.txt
+#RUN /usr/bin/python3 -m pip install Cython==0.29.36  --break-system-packages
+RUN python -m pip install -r requirements.txt --break-system-packages
 COPY cefr_predictor/ cefr_predictor/
 COPY entrypoint.py /
+COPY predict.py /
+COPY fastapi_server.py /
 COPY sample.txt /
 
 EXPOSE 8080
@@ -15,5 +18,5 @@ EXPOSE 8080
 
 ENTRYPOINT ["sleep", "infinity"]
 
-# CMD streamlit run --server.port 8080 --server.enableCORS false CEFR_Predictor.py 
-#--server.maxUploadSize 50 
+# CMD streamlit run --server.port 8080 --server.enableCORS false CEFR_Predictor.py
+#--server.maxUploadSize 50
--- predict.py
+++ predict.py
@@ -1,25 +1,29 @@
-from cog import BasePredictor, Input
+from pydantic import BaseModel
 from typing import List
 from cefr_predictor.inference import Model
 
+class CEFRResponse(BaseModel):
+#    text: str
+    level: str
 
-class Predictor(BasePredictor):
+
+class Predictor:
 
     def setup(self):
         """Load the model into memory to make running multiple predictions efficient"""
         self.model = Model("cefr_predictor/models/xgboost.joblib")
 
     def predict(self,
-                text: str = Input(description="List of texts to evaluate")
-                ) -> List:
+                text: str
+                ) -> CEFRResponse:
         """Run a single prediction on the model"""
         # logging.info(f"Processing texts - {texts}")
         texts=[text]
         preds, probas = self.model.predict_decode(texts)
-
-        results = []
-        for text, pred, proba in zip(texts, preds, probas):
-            row = {"text": text, "level": pred, "scores": proba}
-            results.append(row)
+        # preds=["Sample Result"]
+        result = CEFRResponse(level=preds[0])
+        # for text, pred, proba in zip(texts, preds, probas):
+        #     row = {"text": text, "level": pred, "scores": proba}
+        #     results.append(row)
         # logging.info(f"Request processed. Got results - {results}")
-        return results
+        return result
